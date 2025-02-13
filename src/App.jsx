596ea8b4d35256301a2180c55f00479269513c73
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-02-13 21:01:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import {Link, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+
+function App() {
+  return(
+    <div className="App"> 
+
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/newpost">NewPost</Link></li>
+        <li><Link to="/postpage">PostPage</Link></li>
+      </ul>
+    </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/newpost" element={<NewPost />}/>
+        <Route path="/postpage" element={<PostPage />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+      {/* <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App

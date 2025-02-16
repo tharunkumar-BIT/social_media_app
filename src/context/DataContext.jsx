4962d80886913658b799696068a978514f5ded58
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-02-16 21:57:17 +0530

    created seperate context provider for children

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -8,17 +8,16 @@ import { format } from "date-fns";
 const DataContext = createContext({})
 
 export const DataProvider = ({childern}) => {
-
     const [posts, setPosts] = useState([]);
-  const [search, setSearch] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-  const [editTitle, setEditTitle] = useState('');
-  const [editBody, setEditBody] = useState('');
-  const navigate = useNavigate();
-  const {width} = useWindowSize();
-  const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/posts');
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
+    const [editTitle, setEditTitle] = useState('');
+    const [editBody, setEditBody] = useState('');
+    const navigate = useNavigate();
+    const {width} = useWindowSize();
+    const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/posts');
 
   // useEffect(()=> {
   //   const fetchPosts = async () => {
